507f4b071150ba644fb1a7aa4f85a0c33ad65f1f
Lee SangHun <hoonideal@naver.com>
2023-01-15 02:13:09 +0900

    number_place modified

--- number_place.py
+++ number_place.py
@@ -6,9 +6,6 @@ for test_case in range(1,T+1):
     for j in range(9):
         sdocu.append(list(map(int,(input().split()))))
     for k in range(9):
-        a_sum = 0
-        b_sum = 0
-        c_sum = 0
         b_set = set()
         c_set = set()
         if len(set(sdocu[k]))!=9:
@@ -19,10 +16,7 @@ for test_case in range(1,T+1):
                 result = 0
             else :
                 b_set.add(sdocu[l][k])
-                c_set.add(sdocu[3*k%9+l//3][k//3*3+l%3])
-                a_sum += sdocu[k][l]
-                b_sum += sdocu[l][k]
-                c_sum += sdocu[3*k%9+l//3][k//3*3+l%3]  
-        if a_sum != 45 or b_sum != 45 or c_sum != 45 or len(b_set) !=9 or len(c_set) != 9:
+                c_set.add(sdocu[3*k%9+l//3][k//3*3+l%3]) 
+        if len(b_set) !=9 or len(c_set) != 9:
             result = 0
     print(f"#{test_case} {result}")
