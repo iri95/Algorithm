a6f79ae415eecd0d31bc9457a0054500583d63f8
LeeSanghun <hoonideal@naver.com>
2023-04-16 00:04:12 +0900

    silver dp bj11727_2Xn타일링2

---
+++ Java/Baekjoon/src/silver/bj11727_2Xn타일링2.java
@@ -0,0 +1,18 @@
+package silver;
+
+import java.util.Scanner;
+
+public class bj11727_2Xn타일링2 {
+	public static void main(String[] args) {
+		int T = 10007;
+		Scanner scann = new Scanner(System.in);
+		int N = scann.nextInt();
+		int[] p = new int[N + 2];
+		p[1] = 1;
+		p[2] = 3;
+		for (int i = 3; i < N + 1; i++) {
+			p[i] = ((p[i - 1] % T) + (p[i - 2]*2 % T)) % T;
+		}
+		System.out.println(p[N]);
+	}
+}
