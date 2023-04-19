a4b64b1f4dcb867dbe63fe721265d5166b232bae
LeeSanghun <hoonideal@naver.com>
2023-04-20 00:15:11 +0900

    gold modified bj25682_체스판다시칠하기2

--- Java/Baekjoon/src/Gold/bj25682_체스판다시칠하기2.java
+++ Java/Baekjoon/src/Gold/bj25682_체스판다시칠하기2.java
@@ -5,55 +5,61 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class bj25682_체스판다시칠하기2 {
-	static int N, M, K, cnt;
+	static int N, M, K, result;
 	static boolean[][] map;
-	// 시간초과
+	static int[][] mapB, mapW;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		map = new boolean[N][M];
-		cnt = Integer.MAX_VALUE;
-		for (int i = 0; i < N; i++) {
+		map = new boolean[N + 1][M + 1];
+		mapB = new int[N + 1][M + 1];
+		mapW = new int[N + 1][M + 1];
+		for (int i = 1; i <= N; i++) {
 			String str = br.readLine();
-			for (int j = 0; j < M; j++) {
-				if (str.charAt(j) == 'B') {
+			for (int j = 1; j <= M; j++) {
+				if (str.charAt(j-1) == 'B') {
 					map[i][j] = true;
-				} else {
-					map[i][j] = false;
-				}
-			}
-		}
-		for (int i = 0; i < N - (K-1); i++) {
-			for (int j = 0; j < M - (K-1); j++) {
-				int count = 0;
-				int count2 = 0;
-				boolean tf = map[i][j];
-				boolean tf2 = map[i][j];
-				for (int k = i; k < i + K; k++) {
-					for (int k2 = j; k2 < j + K; k2++) {
-						if(tf == map[k][k2])count++;
-						if(tf2 != map[k][k2])count2++;
-						if(tf == true)tf = false;
-						else tf = true;
-						if(tf2 == true)tf2 = false;
-						else tf2 = true;
-					}
-					if(K%2 == 0) {
-						if(tf == true)tf = false;
-						else tf = true;
-						if(tf2 == true)tf2 = false;
-						else tf2 = true;
-					}
-				}
-				count = Math.min(K*K - count, K*K - count2);
-				cnt = Math.min(count, cnt);
-			}
-		}
-		System.out.println(cnt);
 
+				}
+			}
+		}
+
+		boolean a = false;
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
+				if (map[i][j]) {
+					if (a) {
+						mapW[i][j] = mapW[i - 1][j] + mapW[i][j-1] - mapW[i-1][j-1] + 1;
+						mapB[i][j] = mapB[i - 1][j] + mapB[i][j-1] - mapB[i-1][j-1];
+					} else {
+						mapB[i][j] = mapB[i - 1][j] + mapB[i][j-1] - mapB[i-1][j-1] +1;
+						mapW[i][j] = mapW[i - 1][j] + mapW[i][j-1] - mapW[i-1][j-1] ;
+					}
+				} else {
+					if (a) {
+						mapB[i][j] = mapB[i - 1][j] + mapB[i][j-1] - mapB[i-1][j-1] + 1;
+						mapW[i][j] = mapW[i - 1][j] + mapW[i][j-1] - mapW[i-1][j-1] ;
+					} else {
+						mapW[i][j] = mapW[i - 1][j] + mapW[i][j-1] - mapW[i-1][j-1] + 1;
+						mapB[i][j] = mapB[i - 1][j] + mapB[i][j-1] - mapB[i-1][j-1];
+					}
+				}
+				a = !a;
+			}
+			if (M % 2 == 0) {
+				a = !a;
+			}
+		}
+		result = Integer.MAX_VALUE;
+		for (int i = 0; i <= N - K; i++) {
+			for (int j = 0; j <= M - K; j++) {
+				result = Math.min(result,Math.min(mapB[i+K][j+K] - mapB[i][j+K] - mapB[i+K][j] + mapB[i][j], mapW[i+K][j+K] - mapW[i][j+K] - mapW[i+K][j] + mapW[i][j]));
+			}
+		}
+		System.out.println(result);
 	}
 }
-
