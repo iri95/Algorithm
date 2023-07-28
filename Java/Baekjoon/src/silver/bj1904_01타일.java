890a2eb2cf1c1985ae9c4dcbf5b3c1fa0c4c2d9f
LeeSanghun <hoonideal@naver.com>
2023-07-29 04:29:56 +0900

    Silver DP bj1904_01타일

--- Java/Baekjoon/src/silver/bj1904_01타일.java
+++ Java/Baekjoon/src/silver/bj1904_01타일.java
@@ -9,6 +9,11 @@ public class bj1904_01타일 {
         int N = Integer.parseInt(br.readLine());
         int[] dp = new int[N + 1];
         dp[1] = 1;
+        if(N == 1){
+            System.out.println(1);
+            return;
+        }
+
         dp[2] = 2;
         for (int i = 3; i <= N; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
