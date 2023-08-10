7e886f523c46cce7f0da084799b25b0a7a501821
LeeSanghun <hoonideal@naver.com>
2023-08-11 06:02:10 +0900

    Bronze bj2743_단어길이재기

---
+++ Java/Baekjoon/src/bronze/bj2743_단어길이재기.java
@@ -0,0 +1,12 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bj2743_단어길이재기 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        System.out.println(a.length());
+    }
+}
