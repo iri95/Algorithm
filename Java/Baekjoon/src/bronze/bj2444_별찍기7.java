aa513b573e4d73a938da100bfe1e6c39021a0695
LeeSanghun <hoonideal@naver.com>
2023-08-14 00:00:30 +0900

    Bronze bj2444_별찍기7

---
+++ Java/Baekjoon/src/bronze/bj2444_별찍기7.java
@@ -0,0 +1,29 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bj2444_별찍기7 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * (n-i) - 1 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
