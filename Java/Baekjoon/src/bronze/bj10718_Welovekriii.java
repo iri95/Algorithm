c0c747d0ca810221fe182a76c07d2e02aa55e28b
LeeSanghun <hoonideal@naver.com>
2023-09-05 00:15:45 +0900

    Bronze bj10718_Welovekriii

---
+++ Java/Baekjoon/src/bronze/bj10718_Welovekriii.java
@@ -0,0 +1,10 @@
+package bronze;
+
+public class bj10718_Welovekriii {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            System.out.println("강한친구 대한육군");
+        }
+
+    }
+}
