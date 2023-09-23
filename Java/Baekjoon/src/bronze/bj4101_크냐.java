a91855f584fce0d07eedf23a714fbcaf35f273db
LeeSanghun <hoonideal@naver.com>
2023-09-24 02:38:21 +0900

    Bronze bj4101_크냐

---
+++ Java/Baekjoon/src/bronze/bj4101_크냐.java
@@ -0,0 +1,22 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj4101_크냐 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a == 0 && b == 0) {
+                return;
+            }
+            System.out.println(a > b ? "Yes" : "No");
+        }
+
+    }
+}
+
