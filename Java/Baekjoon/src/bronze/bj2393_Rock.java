c819a6989c172393a2e7a2b085c006aabc41f60c
LeeSanghun <hoonideal@naver.com>
2023-10-14 18:25:20 +0900

    Silver bj2393_Rock

---
+++ Java/Baekjoon/src/bronze/bj2393_Rock.java
@@ -0,0 +1,19 @@
+package bronze;
+
+public class bj2393_Rock {
+    public static void main(String[] args) throws Exception {
+        System.out.println("  ___  ___  ___");
+        System.out.println("  | |__| |__| |");
+        System.out.println("  |           |");
+        System.out.println("   \\_________/");
+        System.out.println("    \\_______/");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |_____|");
+        System.out.println("  __/       \\__");
+        System.out.println(" /             \\");
+        System.out.println("/_______________\\");
+    }
+}
