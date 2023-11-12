6768a0452a25be1be67a80a795a01d334ea4e6c2
LeeSanghun <hoonideal@naver.com>
2023-11-13 00:43:46 +0900

    Bronze bj6996_에너그램

---
+++ Java/Baekjoon/src/bronze/bj6996_에너그램.java
@@ -0,0 +1,32 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class bj6996_에너그램 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String str1 = st.nextToken();
+            String str2 = st.nextToken();
+            String a1 = str1.chars().sorted().collect(StringBuilder::new,
+                            StringBuilder::appendCodePoint,
+                            StringBuilder::append)
+                    .toString();
+            String a2 = str2.chars().sorted().collect(StringBuilder::new,
+                    StringBuilder::appendCodePoint,
+                    StringBuilder::append)
+                    .toString();;
+            if(a1.equals(a2)){
+                System.out.println(str1 + " & " + str2 + " are anagrams.");
+            }else{
+                System.out.println(str1 + " & " + str2 + " are NOT anagrams.");
+            }
+
+        }
+    }
+}
