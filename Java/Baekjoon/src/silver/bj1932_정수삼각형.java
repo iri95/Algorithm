1b8be487c042dc6c282c895b4db141489f6970de
이상훈 <hoonideal@naver.com>
2023-11-30 08:57:20 +0900

    Silver bj1932_정수삼각형

---
+++ Java/Baekjoon/src/silver/bj1932_정수삼각형.java
@@ -0,0 +1,25 @@
+package Baekjoon.src.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj1932_정수삼각형 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N+1][N+1];
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < i+1; j++) {
+                dp[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = Math.max(dp[i - 1][j - 1] + dp[i][j], dp[i - 1][j] + dp[i][j]);
+            }
+        }
+        int max = 0;
+        for (int i = 1; i <= N; i++) {
+            max = Math.max(max, dp[N][i]);
+        }
+        System.out.println(max);
+    }
+}
