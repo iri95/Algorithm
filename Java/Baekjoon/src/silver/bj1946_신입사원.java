ab6b918f7e1cdc9165e96b44a7e0aa80daa3c7b2
LeeSanghun <hoonideal@naver.com>
2023-12-07 10:20:37 +0900

    Silver bj1946_신입사원 modify

--- Java/Baekjoon/src/silver/bj1946_신입사원.java
+++ Java/Baekjoon/src/silver/bj1946_신입사원.java
@@ -12,37 +12,21 @@ public class bj1946_신입사원 {
         StringBuilder sb = new StringBuilder();
         for (int t = 0; t < T; t++) {
             int N = Integer.parseInt(br.readLine());
-            Unit[] list = new Unit[N];
-            for (int i = 0; i < N; i++) {
+            int[] list = new int[N+1];
+            for (int i = 1; i <= N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                list[i] = new Unit(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+                list[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
             }
-            Arrays.sort(list);
             int cnt = 1;
-            int min = list[0].meeting;
-            for (int i = 1; i < N; i++) {
-                if (list[i].meeting < min) {
+            int min = list[1];
+            for (int i = 2; i <= N; i++) {
+                if (list[i] < min) {
                     cnt++;
-                    min = list[i].meeting;
+                    min = list[i];
                 }
             }
             sb.append(cnt).append("\n");
         }
         System.out.println(sb);
-    }
-    public static class Unit implements Comparable<Unit>{
-        int profile;
-        int meeting;
-
-        public Unit(int profile, int meeting) {
-            this.profile = profile;
-            this.meeting = meeting;
-        }
-
-        @Override
-        public int compareTo(Unit o) {
-            if (profile < o.profile) return -1;
-            else return 1;
-        }
     }
 }
