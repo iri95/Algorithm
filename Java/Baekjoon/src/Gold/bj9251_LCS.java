04a44d2f0b8dddd00817e5a96f62dad03cbc7ca1
LeeSanghun <hoonideal@naver.com>
2023-12-09 02:31:28 +0900

    Gold DP bj9251_LCS

---
+++ Java/Baekjoon/src/Gold/bj9251_LCS.java
@@ -0,0 +1,25 @@
+package Gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bj9251_LCS {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] str1 = br.readLine().toCharArray();
+        char[] str2 = br.readLine().toCharArray();
+        int len1 = str1.length;
+        int len2 = str2.length;
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i < len1 + 1; i++) {
+            for (int j = 1; j < len2 + 1; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = Math.max(dp[i][j-1], Math.max(dp[i-1][j], dp[i-1][j-1] + 1));
+                }else{
+                    dp[i][j] = Math.max(dp[i][j-1], Math.max(dp[i-1][j], dp[i - 1][j - 1]));
+                }
+            }
+        }
+        System.out.println(dp[len1][len2]);
+    }
+}
