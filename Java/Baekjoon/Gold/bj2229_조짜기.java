074756eaa914e396627e51689656e9063e2729f7
LeeSanghun <hoonideal@naver.com>
2024-01-22 09:45:58 +0900

    Gold DP bj2229_조짜기

--- Java/Baekjoon/Gold/bj2229_조짜기.java
+++ Java/Baekjoon/Gold/bj2229_조짜기.java
@@ -5,37 +5,24 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class bj2229_조짜기 {
-    static int N;
-    static int[] list, dp;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        list = new int[N];
-        dp = new int[N];
-        for (int i = 0; i < N; i++) {
+        int[] list = new int[N + 1];
+        int[] dp = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
             list[i] = Integer.parseInt(st.nextToken());
         }
-        if (N >= 2) dp[1] = result(0, 1);
-        if (N >= 3) dp[2] = result(0, 2);
-        for (int i = 3; i < N; i++) {
-            dp[i] = result(0, i);
+        for (int i = 2; i <= N; i++) {
+            int min = list[i];
+            int max = list[i];
             for (int j = i - 1; j >= 0; j--) {
-                dp[i] = Math.max(dp[i], dp[j] + result(j + 1, i));
+                if (max < list[j + 1]) max = list[j + 1];
+                if (min > list[j + 1]) min = list[j + 1];
+                dp[i] = Math.max(dp[i], dp[j] + max - min);
             }
         }
-        System.out.println(dp[N - 1]);
+        System.out.println(dp[N]);
     }
-
-    static int result(int minIndex, int maxIndex) {
-        int min = 100001;
-        int max = 0;
-        for (int i = minIndex; i <= maxIndex; i++) {
-            if (list[i] > max) max = list[i];
-            if (list[i] < min) min = list[i];
-        }
-        return Math.abs(max - min);
-    }
-
 }
