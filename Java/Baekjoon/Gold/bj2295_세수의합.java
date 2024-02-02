88b41241d64438decb9bfac22bfe735687b9222d
LeeSanghun <hoonideal@naver.com>
2024-02-03 01:55:59 +0900

    Gold BinarySearch bj2295_세수의합

--- Java/Baekjoon/Gold/bj2295_세수의합.java
+++ Java/Baekjoon/Gold/bj2295_세수의합.java
@@ -15,16 +15,33 @@ public class bj2295_세수의합 {
         Arrays.sort(list);
         for (int i = N - 1; i >= 0; i--) {
             for (int j = i; j >= 0; j--) {
-                int start = 0;
-                int end = j;
-                while (start <= end) {
-                    if (list[start] + list[end] > list[i] - list[j]) end--;
-                    else if (list[start] + list[end] < list[i] - list[j]) start++;
-                    else {
-                        System.out.println(list[i]);
-                        return;
+                // Binary Search 196ms
+                for (int k = 0; k <= j; k++) {
+                    int target = list[i] - list[j] - list[k];
+                    if (target < 0) break;
+                    int start = 0;
+                    int end = k;
+                    while(start <= end){
+                        int mid = (start + end) /2;
+                        if (list[mid] > target)end = mid - 1;
+                        else if (list[mid] < target)start = mid + 1;
+                        else{
+                            System.out.println(list[i]);
+                            return;
+                        }
                     }
                 }
+                // Two Pointer 276ms
+//                int start = 0;
+//                int end = j;
+//                while (start <= end) {
+//                    if (list[start] + list[end] > list[i] - list[j]) end--;
+//                    else if (list[start] + list[end] < list[i] - list[j]) start++;
+//                    else {
+//                        System.out.println(list[i]);
+//                        return;
+//                    }
+//                }
             }
         }
     }
