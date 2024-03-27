705360559a2623c075658e4af8d995cc385268b9
LeeSanghun <hoonideal@naver.com>
2024-03-27 11:16:19 +0900

    Gold Sliding Window bj3078_좋은친구

--- Java/Baekjoon/Gold/bj3078_좋은친구.java
+++ Java/Baekjoon/Gold/bj3078_좋은친구.java
@@ -3,7 +3,7 @@ package Gold;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+// TODO: Sliding Window 정리
 public class bj3078_좋은친구 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
