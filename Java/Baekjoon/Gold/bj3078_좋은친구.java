1aa0cda511ec63ea45e2f1dfd51d34353d08d909
LeeSanghun <hoonideal@naver.com>
2024-03-27 11:15:32 +0900

    Gold Sliding Window bj3078_좋은친구

---
+++ Java/Baekjoon/Gold/bj3078_좋은친구.java
@@ -0,0 +1,34 @@
+package Gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj3078_좋은친구 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        String[] list = new String[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = br.readLine();
+        }
+        int[] count = new int[21];
+        long answer = 0;
+        for (int i = 0; i <= K; i++) {
+            if (count[list[i].length()] > 0) {
+                answer += count[list[i].length()];
+            }
+            count[list[i].length()]++;
+        }
+        for (int i = K + 1; i < N; i++) {
+            count[list[i - K - 1].length()]--;
+            if (count[list[i].length()] > 0) {
+                answer += count[list[i].length()];
+            }
+            count[list[i].length()]++;
+        }
+        System.out.println(answer);
+    }
+}
