75065753dd609b3f769422fb5a57b2b4e4202987
LeeSanghun <hoonideal@naver.com>
2024-04-01 10:59:23 +0900

    Gold BFS bj10282_해킹

--- Java/Baekjoon/bj10282_해킹.java
+++ Java/Baekjoon/bj10282_해킹.java
@@ -8,13 +8,10 @@ public class bj10282_해킹 {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-        int n;
-        int d;
-        int c;
-        int sum;
-        int count;
+        int n, d, c, sum, count, a, b, s;
         List<int[]>[] after;
         boolean[] visited;
+        Queue<int[]> queue = new PriorityQueue<>(Comparator.comparing(o -> o[1]));
         while (T-- > 0) {
             st = new StringTokenizer(br.readLine());
             n = Integer.parseInt(st.nextToken());
@@ -24,27 +21,26 @@ public class bj10282_해킹 {
             count = 0;
             after = new ArrayList[n + 1];
             visited = new boolean[n + 1];
-            Queue<int[]> queue = new PriorityQueue<>(Comparator.comparing(o -> o[1]));
-            queue.add(new int[] {c, 0});
             for (int i = 0; i <= n; i++) {
                 after[i] = new ArrayList<>();
             }
             for (int i = 0; i < d; i++) {
                 st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                int s = Integer.parseInt(st.nextToken());
+                a = Integer.parseInt(st.nextToken());
+                b = Integer.parseInt(st.nextToken());
+                s = Integer.parseInt(st.nextToken());
                 after[b].add(new int[]{a, s});
             }
+            queue.add(new int[]{c, 0});
             while (!queue.isEmpty()) {
                 int[] now = queue.poll();
                 if (visited[now[0]]) continue;
                 sum = now[1];
                 count++;
                 visited[now[0]] = true;
-                for (int[] p: after[now[0]]) {
+                for (int[] p : after[now[0]]) {
                     if (visited[p[0]]) continue;
-                    queue.add(new int[] {p[0], p[1] + now[1]});
+                    queue.add(new int[]{p[0], p[1] + now[1]});
                 }
             }
             sb.append(count).append(" ").append(sum).append("\n");
