65df4cdcbb7e5784c04d929bcd931a5105b15d8d
LeeSanghun <hoonideal@naver.com>
2024-04-27 05:11:41 +0900

    Gold DP bj13302_리조트(28점)

--- Java/Baekjoon/Gold/bj13302_리조트.java
+++ Java/Baekjoon/Gold/bj13302_리조트.java
@@ -7,8 +7,8 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class bj13302_리조트 {
-    static int[] price = {10000, 25000, 37000};
-    static int[] coupon = {0, 1, 2};
+    static int[] price = {0, 10000, 0, 25000, 0, 37000};
+    static int[] coupon = {0, 0, 0, 1, 0, 2};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,7 +16,7 @@ public class bj13302_리조트 {
         int N = Integer.parseInt(st.nextToken()),
                 M = Integer.parseInt(st.nextToken());
 
-        int[][] dp = new int[N + 1][41]; // i번쨰 날, 이번 일권, 이전 일권, sum coupon
+        int[][] dp = new int[N + 1][41];
         for (int i = 0; i <= N; i++) {
             Arrays.fill(dp[i], 1_000_000);
         }
@@ -34,11 +34,8 @@ public class bj13302_리조트 {
             for (int j = 1; j <= 5; j += 2) {
                 if (i - j < 0) continue;
                 for (int k = 0; k <= 40; k++) {
-                    if (k - coupon[j / 2] < 0) continue;
-                    dp[i][k] = Math.min(dp[i][k], dp[i - j][k - coupon[j / 2]] + price[j / 2]);
-                }
-                for (int k = 0; k <= 37; k++) {
-                    dp[i][k] = Math.min(dp[i][k], dp[i - 1][k + 3]);
+                    if (k - coupon[j] >= 0) dp[i][k] = Math.min(dp[i][k], dp[i - j][k - coupon[j]] + price[j]);
+                    if (k + 3 <= 40) dp[i][k] = Math.min(dp[i][k], dp[i - 1][k + 3]);
                 }
             }
         }
