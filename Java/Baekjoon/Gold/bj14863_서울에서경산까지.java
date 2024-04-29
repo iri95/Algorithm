80c8fd2141f0783c28eeede2f78b2832f6766f3c
LeeSanghun <hoonideal@naver.com>
2024-04-29 23:16:34 +0900

    Gold DP bj14863_서울에서경산까지

--- Java/Baekjoon/Gold/bj14863_서울에서경산까지.java
+++ Java/Baekjoon/Gold/bj14863_서울에서경산까지.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// 408ms -> 324ms : 입력과 로직을 따로 처리한 경우 시간이 줄어듬.
 public class bj14863_서울에서경산까지 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,20 +18,22 @@ public class bj14863_서울에서경산까지 {
             Arrays.fill(dp[i], -1);
         }
         dp[0][0] = 0;
-        int t1, t2, m1, m2;
+        int[][] info = new int[N + 1][4];
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            t1 = Integer.parseInt(st.nextToken());
-            m1 = Integer.parseInt(st.nextToken());
-            t2 = Integer.parseInt(st.nextToken());
-            m2 = Integer.parseInt(st.nextToken());
+            info[i][0] = Integer.parseInt(st.nextToken());
+            info[i][1] = Integer.parseInt(st.nextToken());
+            info[i][2] = Integer.parseInt(st.nextToken());
+            info[i][3] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 1; i <= N; i++) {
             for (int j = 0; j <= K; j++) {
                 if (dp[i - 1][j] == -1) continue;
-                if (j + t1 <= K) {
-                    dp[i][j + t1] = Math.max(dp[i][j + t1], dp[i - 1][j] + m1);
+                if (j + info[i][0] <= K) {
+                    dp[i][j + info[i][0]] = Math.max(dp[i][j + info[i][0]], dp[i - 1][j] + info[i][1]);
                 }
-                if (j + t2 <= K) {
-                    dp[i][j + t2] = Math.max(dp[i][j + t2], dp[i - 1][j] + m2);
+                if (j + info[i][2] <= K) {
+                    dp[i][j + info[i][2]] = Math.max(dp[i][j + info[i][2]], dp[i - 1][j] + info[i][3]);
                 }
             }
         }
