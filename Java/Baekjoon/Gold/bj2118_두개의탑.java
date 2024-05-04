845d03c1355447ea53eb2c1f81c99a5583a4cf29
LeeSanghun <hoonideal@naver.com>
2024-05-04 12:47:43 +0900

    Gold Prefix Sum + Two Pointer bj2118_두개의탑

--- Java/Baekjoon/Gold/bj2118_두개의탑.java
+++ Java/Baekjoon/Gold/bj2118_두개의탑.java
@@ -4,32 +4,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// https://www.acmicpc.net/source/77788398
 public class bj2118_두개의탑 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int lenSum = 0;
-        int[] lens = new int[N];
-        for (int i = 0; i < N; i++) {
-            lens[i] = Integer.parseInt(br.readLine());
-            lenSum += lens[i];
+        int[] lens = new int[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            int len = Integer.parseInt(br.readLine());
+            lens[i] = lens[i - 1] + len;
+            lenSum += len;
         }
         int ans = 0;
-        int start = 0;
         int end = 0;
-        int sum = lens[0];
-        while (start < N) {
-            if (sum <= lenSum / 2){
-                ans = Math.max(sum, ans);
-                end++;
-                sum += lens[end % N];
-            }else{
-                sum -= lens[start];
+        int start = 0;
+        while (end <= N) {
+            int right = lens[end] - lens[start];
+            int left = lenSum - right;
+            if (right >= left) {
+                ans = Math.max(ans, left);
                 start++;
+            } else {
+                ans = Math.max(ans, right);
+                end++;
             }
         }
-
         System.out.println(ans);
     }
 }
