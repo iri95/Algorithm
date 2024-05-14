7a33ede2df1dde54bc881d49b75a5b5d14687016
LeeSanghun <hoonideal@naver.com>
2024-05-14 09:55:37 +0900

    Gold Bit Masking + Prefix Sum bj9527_1의개수세기

---
+++ Java/Baekjoon/Gold/bj9527_1의개수세기.java
@@ -0,0 +1,35 @@
+package Gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://gre-eny.tistory.com/209
+public class bj9527_1의개수세기 {
+    static long[] bit = new long[55]; // 2^ 54 > 10 ^ 16
+    public static void main(String[] args) throws Exception {
+        setBit();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        System.out.println(countBit(b) - countBit(a - 1));
+    }
+    static void setBit(){
+        bit[0] = 1;
+        for (int i = 1; i < 55; i++) {
+            bit[i] = bit[i - 1] * 2 + (1L << i);
+        }
+    }
+    static long countBit(long x){
+        long result = x & 1;
+        for (int i = 54; i > 0; i--) {
+            if ((x & (1L << i)) > 0){
+                result += bit[i - 1] + (x - (1L << i) + 1);
+                x -= (1L << i);
+            }
+        }
+
+        return result;
+    }
+}
