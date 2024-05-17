8155ab7004dcb4f46018dca4285133b5e3df3827
LeeSanghun <hoonideal@naver.com>
2024-05-17 09:51:11 +0900

    Gold Prefix Sum + Map bj_2015_수들의합4

--- Java/Baekjoon/Gold/bj_2015_수들의합4.java
+++ Java/Baekjoon/Gold/bj_2015_수들의합4.java
@@ -2,28 +2,26 @@ package Gold;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
-// 시간초과, 자료구조 공부 후 다시 풀어보기
 public class bj_2015_수들의합4 {
-	static int N, K, result;
-	static int[] A;
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		A = new int[N+1];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			A[i] = Integer.parseInt(st.nextToken()) + A[i-1];
-		}
-		for (int i = 1; i <= N; i++) {
-			for (int j = i; j <= N; j++) {
-				if(A[j] - A[i-1] == K)result++;
-			}
-		}
-		System.out.println(result);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int sum = 0;
+		long result = 0;
+		Map<Integer, Integer> map = new HashMap<>();
+		map.put(0, 1);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            sum += Integer.parseInt(st.nextToken());
+			result += map.getOrDefault(sum - K, 0);
+			map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        System.out.println(result);
+    }
 }
