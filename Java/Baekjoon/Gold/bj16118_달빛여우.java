99197123f65fefc6d80284fc87e61651e7678e4e
LeeSanghun <hoonideal@naver.com>
2024-05-20 10:52:42 +0900

    Gold Dijkstra bj16118_달빛여우

--- Java/Baekjoon/Gold/bj16118_달빛여우.java
+++ Java/Baekjoon/Gold/bj16118_달빛여우.java
@@ -10,6 +10,11 @@ public class bj16118_달빛여우 {
         double cost;
         double div;
 
+        public Node(int num, double cost) {
+            this.num = num;
+            this.cost = cost;
+        }
+
         public Node(int num, double cost, double div) {
             this.num = num;
             this.cost = cost;
@@ -22,7 +27,7 @@ public class bj16118_달빛여우 {
     }
 
     static int N, M;
-    static double[][] distance;
+    static double[] wolfDistance, foxDistance;
     static List<Node>[] route;
 
     public static void main(String[] args) throws Exception {
@@ -43,33 +48,51 @@ public class bj16118_달빛여우 {
             route[b].add(new Node(a, d, 0));
         }
 
-        distance = new double[3][N + 1];
-        for (int i = 1; i <= 2; i++) {
-            Arrays.fill(distance[i], Integer.MAX_VALUE);
-            distance[i][1] = 0;
-        }
-        dijkstra(1);
-        dijkstra(2);
+        foxDistance = new double[N + 1];
+        wolfDistance = new double[N + 1];
+        Arrays.fill(foxDistance, Integer.MAX_VALUE);
+        Arrays.fill(wolfDistance, Integer.MAX_VALUE);
+        foxDistance[1] = 0;
+        wolfDistance[1] = 0;
+        foxDijkstra();
+        wolfDijkstra();
         int answer = 0;
         for (int i = 1; i <= N; i++) {
-            if (distance[1][i] < distance[2][i]) answer++;
+            if (foxDistance[i] < wolfDistance[i]) answer++;
         }
         System.out.println(answer);
 
     }
 
-    static void dijkstra(int div) {
+    static void foxDijkstra() {
+        boolean[] visited = new boolean[N + 1];
+        Queue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(1, 0));
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+            if (visited[now.num]) continue;
+            visited[now.num] = true;
+            for (Node next : route[now.num]) {
+                if (foxDistance[next.num] > foxDistance[now.num] + next.cost) {
+                    foxDistance[next.num] = foxDistance[now.num] + next.cost;
+                    pq.add(new Node(next.num, foxDistance[next.num]));
+                }
+            }
+        }
+    }
+
+    static void wolfDijkstra() {
         boolean[][] visited = new boolean[3][N + 1];
         Queue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(1, 0, div));
+        pq.add(new Node(1, 0, 2));
         while (!pq.isEmpty()) {
             Node now = pq.poll();
             if (visited[(int) now.div][now.num]) continue;
             visited[(int) now.div][now.num] = true;
             for (Node next : route[now.num]) {
-                if (distance[div][next.num] > now.cost + next.cost / now.div) {
-                    distance[div][next.num] = now.cost + next.cost / now.div;
-                    pq.add(new Node(next.num, distance[div][next.num], (double) 1 / now.div));
+                if (wolfDistance[next.num] > now.cost + next.cost / now.div) {
+                    wolfDistance[next.num] = now.cost + next.cost / now.div;
+                    pq.add(new Node(next.num, wolfDistance[next.num], (double) 1 / now.div));
                 } else if (!visited[(int) ((double) 1 / now.div)][next.num]) {
                     pq.add(new Node(next.num, now.cost + next.cost / now.div, (double) 1 / now.div));
                 }
