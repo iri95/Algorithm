338de9d7942c6611edcbea8e076b53e08275b966
LeeSanghun <hoonideal@naver.com>
2024-05-24 16:14:09 +0900

    Gold DP bj2157_여행

--- Java/Baekjoon/Gold/bj2157_여행.java
+++ Java/Baekjoon/Gold/bj2157_여행.java
@@ -4,16 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
+// https://www.acmicpc.net/source/77431815
 public class bj2157_여행 {
-    static class Node {
-        int num;
-        int cost;
-
-        public Node(int num, int cost) {
-            this.num = num;
-            this.cost = cost;
-        }
-    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,33 +13,27 @@ public class bj2157_여행 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        List<Node>[] nodes = new ArrayList[N + 1];
-        int[][] dp = new int[N + 1][M + 1];
-        for (int i = 0; i <= N; i++) {
-            nodes[i] = new ArrayList<>();
-        }
-        dp[1][1] = 0;
+
+        int[][] map = new int[N + 1][N + 1];
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            if (s > e) continue;
-            nodes[s].add(new Node(e, c));
+            map[s][e] = Math.max(map[s][e], c);
         }
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(1);
-        int cnt = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            cnt++;
-            while (size-- > 0) {
-                int now = q.poll();
-                for (Node next : nodes[now]) {
-                    if (dp[next.num][cnt + 1] < dp[now][cnt] + next.cost) {
-                        dp[next.num][cnt + 1] = dp[now][cnt] + next.cost;
-                        if (cnt + 1 < M) q.add(next.num);
-                    }
+        int[][] dp = new int[N + 1][M + 1];
+        for (int i = 0; i <= N; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        dp[1][1] = 0;
+        for (int start = 1; start < N; start++) {
+            for (int end = start + 1; end <= N; end++) {
+                if (map[start][end] == 0) continue;
+
+                for (int depth = 1; depth < M; depth++) {
+                    if (dp[start][depth] == -1) continue;
+                    dp[end][depth + 1] = Math.max(dp[end][depth + 1], dp[start][depth] + map[start][end]);
                 }
             }
         }
