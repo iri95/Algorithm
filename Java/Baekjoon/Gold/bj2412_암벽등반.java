c36151c4420e7fdbeb75a7d83abe2a897438c081
LeeSanghun <hoonideal@naver.com>
2024-05-28 09:40:57 +0900

    Gold BFS bj2412_암벽등반

--- Java/Baekjoon/Gold/bj2412_암벽등반.java
+++ Java/Baekjoon/Gold/bj2412_암벽등반.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class bj2412_암벽등반 {
-    static class Point{
+    static class Point {
         int x;
         int y;
         boolean visited;
@@ -14,34 +14,35 @@ public class bj2412_암벽등반 {
             this.x = x;
             this.y = y;
         }
-
     }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int T = Integer.parseInt(st.nextToken());
-        Map<Integer, List<Point>> map = new HashMap<>();
+        List<Point>[] lists = new ArrayList[T + 1];
+        for (int i = 0; i <= T; i++) {
+            lists[i] = new ArrayList<>();
+        }
         while (n-- > 0) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            if (!map.containsKey(x)) map.put(x, new ArrayList<>());
-            map.get(x).add(new Point(x, y));
+            lists[y].add(new Point(x, y));
         }
         Queue<Point> q = new ArrayDeque<>();
         q.add(new Point(0, 0));
         int cnt = 0;
         int ans = -1;
-        bfs : while(!q.isEmpty()){
+        bfs: while (!q.isEmpty()) {
             cnt++;
             int size = q.size();
             while (size-- > 0) {
                 Point p = q.poll();
-                for (int nx = p.x - 2; nx <= p.x + 2 ; nx++) {
-                    if (!map.containsKey(nx)) continue;
-                    for (Point np : map.get(nx)) {
-                        if (np.visited || np.y > p.y + 2 || np.y < p.y - 2) continue;
+                for (int ny = Math.max(p.y - 2, 0); ny <= p.y + 2 && ny <= T; ny++) {
+                    for (Point np : lists[ny]) {
+                        if (np.visited || np.x > p.x + 2 || np.x < p.x - 2) continue;
                         np.visited = true;
                         q.add(np);
                         if (np.y >= T) {
