7bf9d0286997b69c308633889a469c02c184b172
LeeSanghun <hoonideal@naver.com>
2024-06-06 04:26:08 +0900

    Gold BFS bj16397_탈출

---
+++ Java/Baekjoon/Gold/bj16397_탈출.java
@@ -0,0 +1,58 @@
+package Gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class bj16397_탈출 {
+    static int N, T, G, INF = 100_000;
+    static boolean[] dp = new boolean[100_000];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+        G = Integer.parseInt(st.nextToken());
+        dp[N] = true;
+
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(N);
+        int count = 0;
+        int ans = INF;
+        while (!q.isEmpty() && count <= T) {
+            int size = q.size();
+            while(size-- > 0) {
+                int n = q.poll();
+                if (n == G) {
+                    System.out.println(count);
+                    return;
+                }
+                if (n + 1 >= INF) continue;
+                if (!dp[n + 1]) {
+                    dp[n + 1] = true;
+                    q.add(n + 1);
+                }
+
+                int next = n * 2;
+                if (next > 99999) continue;
+                else if (next > 9999) next -= 10000;
+                else if (next > 999) next -= 1000;
+                else if (next > 99) next -= 100;
+                else if (next > 9) next -= 10;
+                else next--;
+
+                if (next < 0) continue;
+
+                if (!dp[next]) {
+                    dp[next] = true;
+                    q.add(next);
+                }
+            }
+            count++;
+        }
+        System.out.println("ANG");
+    }
+}
