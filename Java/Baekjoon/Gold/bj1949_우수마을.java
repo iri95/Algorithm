c291ccad3cee36aebab83ac392a411e8e5aef3ca
LeeSanghun <hoonideal@naver.com>
2024-07-06 03:44:13 +0900

    Gold Tree + DP + DFS bj1949_우수마을

--- Java/Baekjoon/Gold/bj1949_우수마을.java
+++ Java/Baekjoon/Gold/bj1949_우수마을.java
@@ -6,18 +6,19 @@ import java.util.*;
 
 public class bj1949_우수마을 {
     static int N;
-    static int[] person;
+    static boolean[] visited;
     static List<Integer>[] nodes;
     static int[][] dp;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        person = new int[N + 1];
         nodes = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        dp = new int[N + 1][2]; // 0 : 우수마을
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            person[i] = Integer.parseInt(st.nextToken());
+            dp[i][0] += Integer.parseInt(st.nextToken());
             nodes[i] = new ArrayList<>();
         }
 
@@ -28,21 +29,17 @@ public class bj1949_우수마을 {
             nodes[a].add(b);
             nodes[b].add(a);
         }
-        dp = new int[N + 1][2]; // 0 : 우수마을
-        System.out.println(Math.max(dfs(1, 0, 0), dfs(1, 1, 0)));
+        dfs(1);
+        System.out.println(Math.max(dp[1][0], dp[1][1]));
     }
 
-    private static int dfs(int now, int state, int parent) {
-        if (dp[now][state] != 0) return dp[now][state];
-        if (state == 0) {
-            for (int next : nodes[now])
-                if (next != parent)
-                    dp[now][0] += dfs(next, 1, now);
-            dp[now][0] += person[now];
-        } else
-            for (int next : nodes[now])
-                if (next != parent)
-                    dp[now][1] += Math.max(dfs(next, 0, now), dfs(next, 1, now));
-        return dp[now][state];
+    private static void dfs(int now) {
+        visited[now] = true;
+        for (int next : nodes[now]) {
+            if (visited[next]) continue;
+            dfs(next);
+            dp[now][0] += dp[next][1];
+            dp[now][1] += Math.max(dp[next][0], dp[next][1]);
+        }
     }
 }
