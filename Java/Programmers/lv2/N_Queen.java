ab8d301273c47468a4671ee6159c5dcfed745b2e
LeeSanghun <hoonideal@naver.com>
2024-07-11 10:01:00 +0900

    lv2 Back Tracking N_Queen

--- Java/Programmers/lv2/N_Queen.java
+++ Java/Programmers/lv2/N_Queen.java
@@ -5,16 +5,16 @@ public class N_Queen {
     static boolean[] row;
     static boolean[] rCross;
     static boolean[] lCross;
-    public static int solution(int n) {
+    public int solution(int n) {
         row = new boolean[n];
         rCross = new boolean[2 * n - 1];
         lCross = new boolean[2 * n - 1];
-        recursion(0, n, 0);
+        recursion(0, n);
         return answer;
     }
 
-    private static void recursion(int cnt, int n, int x){
-        if(cnt == n){
+    private static void recursion(int x, int n){
+        if(x == n){
             answer++;
             return;
         }
@@ -23,7 +23,7 @@ public class N_Queen {
             int lc = x + i;
             if(row[i] || rCross[rc] || lCross[lc]) continue;
             row[i] = true; rCross[rc] = true; lCross[lc] = true;
-            recursion(cnt + 1, n, x + 1);
+            recursion(x + 1, n);
             row[i] = false; rCross[rc] = false; lCross[lc] = false;
         }
     }
