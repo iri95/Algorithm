34c2a9c747d875043e4128a589f45cae35adc33b
LeeSanghun <hoonideal@naver.com>
2024-07-19 10:51:14 +0900

    Gold Back Tracking bj1799_비숍

--- Java/Baekjoon/Gold/bj1799_비숍.java
+++ Java/Baekjoon/Gold/bj1799_비숍.java
@@ -2,42 +2,41 @@ package Gold;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class bj1799_비숍 {
     static int N;
     static boolean[] right;
-    static int[][] map;
+    static boolean[][] map;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
+        map = new boolean[N][N];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) map[i][j] = st.nextToken().equals("0");
         }
         right = new boolean[2 * N - 1];
-        System.out.println(recursion(0));
+        System.out.println(recursion(0) + recursion(1));
     }
 
     private static int recursion(int x) {
-        if (x >= 2 * N - 1) {
-            return 0;
+        if (x >= 2 * N - 1) return 0;
+        int y = 0;
+        if (x > N - 1){
+            y = x - N + 1;
+            x = N - 1;
         }
-        int y = Math.max(x - N + 1, 0);
-        x = Math.min(x, N - 1);
         int max = -1;
         while (x >= 0 && y < N) {
-            if (map[x][y] == 1 && !right[N - 1 - (x - y)]) {
+            if (!map[x][y] && !right[N - 1 - (x - y)]) {
                 right[N - 1 - (x - y)] = true;
-                max = Math.max(max, recursion(x + y + 1) + 1);
+                max = Math.max(max, recursion(x + y + 2) + 1);
                 right[N - 1 - (x - y)] = false;
             }
-            x--;
-            y++;
+            x--; y++;
         }
-        return max < 0 ? recursion(x + y + 1) : max;
+        return max < 0 ? recursion(x + y + 2) : max;
     }
 }
