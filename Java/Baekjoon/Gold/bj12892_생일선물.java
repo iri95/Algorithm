e2fda46f080d172df87df0e9299cd73e9a3a72a6
LeeSanghun <hoonideal@naver.com>
2024-07-23 10:03:16 +0900

    Gold Two Pointer bj12892_생일선물

--- Java/Baekjoon/Gold/bj12892_생일선물.java
+++ Java/Baekjoon/Gold/bj12892_생일선물.java
@@ -7,34 +7,30 @@ import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class bj12892_생일선물 {
-    static int N, D;
-    static int[][] list;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        D = Integer.parseInt(st.nextToken());
-        list = new int[N + 1][2];
+        int N = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int[][] list = new int[N + 1][2];
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             list[i][0] = Integer.parseInt(st.nextToken());
             list[i][1] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(list, Comparator.comparing(o -> o[0]));
-        long max = 0;
         int index = 1;
-        long left = 0;
-        long right = 0;
-        for (int i = 1; i <= N; i++) {
+        long sum = 0;
+        long max = 0;
+        for (int i = 1; i <= N && index <= N; i++) {
             while (index <= N) {
                 if (list[index][0] - list[i][0] < D) {
-                    right += list[index][1];
+                    sum += list[index][1];
                     index++;
                 } else break;
             }
-            max = Math.max(max, right - left);
-            left += list[i][1];
+            max = Math.max(max, sum);
+            sum -= list[i][1];
         }
         System.out.println(max);
 
