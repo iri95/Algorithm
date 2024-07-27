16e80a33ea3c83aa8026ddeab6d91ce3de2f3f24
LeeSanghun <hoonideal@naver.com>
2024-07-28 00:49:15 +0900

    Silver Floyd Warshall bj1389_케빈베이컨의6단계법칙

--- Java/Baekjoon/silver/bj1389_케빈베이컨의6단계법칙.java
+++ Java/Baekjoon/silver/bj1389_케빈베이컨의6단계법칙.java
@@ -5,53 +5,45 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class bj1389_케빈베이컨의6단계법칙 {
-    static int N;
-    static List<Integer>[] list;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        list = new ArrayList[N + 1];
-        for (int i = 0; i <= N; i++) list[i] = new ArrayList<>();
+        int[][] map = new int[N + 1][N + 1];
+        int INF = 5001;
+        for (int i = 0; i <= N; i++) {
+            Arrays.fill(map[i], INF);
+            map[i][i] = 0;
+        }
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            list[a].add(b);
-            list[b].add(a);
+            map[a][b] = 1;
+            map[b][a] = 1;
         }
-        int min = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                for (int k = 1; k <= N; k++) {
+                    int len = map[j][i] + map[i][k];
+                    if (map[j][k] > len){
+                        map[j][k] = len;
+                    }
+                }
+            }
+        }
         int num = 0;
         for (int i = 1; i <= N; i++) {
-            int count = bfs(i);
-            if (min > count) {
-                min = count;
+            int sum = 0;
+            for (int j = 1; j <= N; j++) sum += map[i][j];
+            if (sum < INF){
+                INF = sum;
                 num = i;
             }
         }
         System.out.println(num);
-    }
-    static int bfs(int n){
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(n);
-        boolean[] visited = new boolean[N + 1];
-        visited[n] = true;
-        int sum = 0;
-        int cnt = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            cnt++;
-            while (size-- > 0) {
-                int num = q.poll();
-                for (int k: list[num]) {
-                    if (visited[k]) continue;
-                    sum += cnt;
-                    visited[k] = true;
-                    q.add(k);
-                }
-            }
-        }
-        return sum;
     }
 }
