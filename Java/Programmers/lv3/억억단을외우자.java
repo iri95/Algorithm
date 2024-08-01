3e19862d59be21bf30fc7b40f2f0bafbd000ff06
LeeSanghun <hoonideal@naver.com>
2024-08-01 10:22:39 +0900

    lv3 Math 억억단을외우자

--- Java/Programmers/lv3/억억단을외우자.java
+++ Java/Programmers/lv3/억억단을외우자.java
@@ -6,19 +6,19 @@ public class 억억단을외우자 {
             int[] answer = new int[starts.length];
             int[] count = new int[e + 1];
             count[1] = 1;
-            for(int i = 2; i <= e; i++){
+            for (int i = 2; i <= e; i++) {
                 count[i] += 2;
-                for(int j = i * 2; j <= e; j += i) count[j]++;
+                for (int j = i * 2; j <= e; j += i) count[j]++;
             }
 
             int[] max = new int[e + 1];
             max[e] = e;
-            for(int i = e - 1; i > 0; i--){
-                if(count[i] >= count[max[i+1]]) max[i] = i;
+            for (int i = e - 1; i > 0; i--) {
+                if (count[i] >= count[max[i + 1]]) max[i] = i;
                 else max[i] = max[i + 1];
             }
 
-            for(int i = 0; i < starts.length; i++){
+            for (int i = 0; i < starts.length; i++) {
                 answer[i] = max[starts[i]];
             }
 
