f95d01adad8542775dcbed8171c171b594115a6d
LeeSanghun <hoonideal@naver.com>
2024-08-05 09:59:41 +0900

    Gold DP bj1943_동전분배

--- Java/Baekjoon/Gold/bj1943_동전분배.java
+++ Java/Baekjoon/Gold/bj1943_동전분배.java
@@ -2,9 +2,9 @@ package Gold;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-// 시간 초과
 public class bj1943_동전분배 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,40 +14,34 @@ public class bj1943_동전분배 {
             int[] count = new int[n];
             int[] account = new int[n];
             int sum = 0;
-            boolean[] dp = new boolean[100001];
-            dp[0] = true;
             for (int i = 0; i < n; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 account[i] = Integer.parseInt(st.nextToken());
                 count[i] = Integer.parseInt(st.nextToken());
                 sum += account[i] * count[i];
-                for (int j = 0; j < count[i]; j++) {
-                    dp[account[i] * j] = true;
-                }
             }
-            if (sum % 2 == 1) {
+            if (sum % 2 != 0) {
                 sb.append(0).append("\n");
-                continue;
-            } else if (dp[sum / 2]) {
-                sb.append(1).append("\n");
                 continue;
             }
 
+            boolean[] dp = new boolean[sum / 2 + 1];
+            dp[0] = true;
+
+            int[] cnt = new int[sum / 2 + 1];
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j < count[i]; j++) {
-                    for (int k = sum / 2; k - account[i] >= 0; k--) {
-                        if (dp[k - account[i]]) {
-                            dp[k] = true;
-                        }
+                Arrays.fill(cnt, 0);
+                for (int k = 0; k + account[i] <= sum / 2; k++) {
+                    if (dp[k + account[i]]) continue;
+                    if (dp[k] && cnt[k] < count[i]) {
+                        dp[k + account[i]] = true;
+                        cnt[k + account[i]] = cnt[k] + 1;
                     }
                 }
             }
 
-            if (dp[sum / 2]) {
-                sb.append(1).append("\n");
-            } else {
-                sb.append(0).append("\n");
-            }
+            if (dp[sum / 2]) sb.append(1).append("\n");
+            else sb.append(0).append("\n");
         }
         System.out.println(sb);
     }
