6cb79cccf0a9e115dee42bfea57fc008c688c01b
LeeSanghun <hoonideal@naver.com>
2024-08-07 09:29:50 +0900

    Silver String bj1283_단축키지정

---
+++ Java/Baekjoon/silver/bj1283_단축키지정.java
@@ -0,0 +1,51 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class bj1283_단축키지정 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            String[] strs = br.readLine().split(" ");
+            int n = strs.length;
+            boolean flag = false;
+            for (int j = 0; j < n; j++) {
+                char ch = Character.toUpperCase(strs[j].charAt(0));
+                if (!set.contains(ch)) {
+                    flag = true;
+                    set.add(ch);
+                    strs[j] = "[" + strs[j].charAt(0) + "]" + strs[j].substring(1);
+                    break;
+                }
+            }
+            if (flag) {
+                for (int j = 0; j < n; j++) {
+                    sb.append(strs[j]).append(" ");
+                }
+                sb.append("\n");
+                continue;
+            }
+            flag = false;
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < strs[j].length() && !flag; k++) {
+                    char ch = Character.toUpperCase(strs[j].charAt(k));
+                    if (!set.contains(ch)) {
+                        flag = true;
+                        set.add(ch);
+                        strs[j] = strs[j].substring(0, k) + "[" + strs[j].charAt(k) + "]" + (k < strs[j].length() - 1 ? strs[j].substring(k + 1) : "");
+                        break;
+                    }
+                }
+                sb.append(strs[j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
