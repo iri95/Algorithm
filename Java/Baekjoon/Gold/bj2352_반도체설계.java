fb4326db3e8a3272e6afe905241c95ab8defb020
LeeSanghun <hoonideal@naver.com>
2024-08-13 09:45:21 +0900

    Gold Binary Search + Array List bj2352_반도체설계

--- Java/Baekjoon/Gold/bj2352_반도체설계.java
+++ Java/Baekjoon/Gold/bj2352_반도체설계.java
@@ -9,20 +9,19 @@ public class bj2352_반도체설계 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n + 1];
-        for (int i = 1; i <= n; i++) arr[i] = Integer.parseInt(st.nextToken());
         List<Integer> list = new ArrayList<>();
-        list.add(arr[1]);
-        for (int i = 2; i <= n; i++) {
+        list.add(Integer.parseInt(st.nextToken()));
+        for (int i = 1; i < n; i++) {
+            int value = Integer.parseInt(st.nextToken());
             int s = 0;
             int e = list.size();
             while (s < e) {
                 int mid = (s + e) / 2;
-                if (list.get(mid) < arr[i]) s = mid + 1;
+                if (list.get(mid) < value) s = mid + 1;
                 else e = mid;
             }
-            if (s == list.size()) list.add(arr[i]);
-            else list.set(s, arr[i]);
+            if (s == list.size()) list.add(value);
+            else list.set(s, value);
         }
         System.out.println(list.size());
     }
