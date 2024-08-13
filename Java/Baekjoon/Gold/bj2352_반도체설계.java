8e5780a5fb6802bc48a7467175e1cb6321a5218e
LeeSanghun <hoonideal@naver.com>
2024-08-13 09:32:01 +0900

    Gold Binary Search + Array List bj2352_반도체설계

--- Java/Baekjoon/Gold/bj2352_반도체설계.java
+++ Java/Baekjoon/Gold/bj2352_반도체설계.java
@@ -14,9 +14,15 @@ public class bj2352_반도체설계 {
         List<Integer> list = new ArrayList<>();
         list.add(arr[1]);
         for (int i = 2; i <= n; i++) {
-            int value = Collections.binarySearch(list, arr[i]) * (-1) - 1;
-            if (value == list.size()) list.add(arr[i]);
-            else list.set(value, arr[i]);
+            int s = 0;
+            int e = list.size();
+            while (s < e) {
+                int mid = (s + e) / 2;
+                if (list.get(mid) < arr[i]) s = mid + 1;
+                else e = mid;
+            }
+            if (s == list.size()) list.add(arr[i]);
+            else list.set(s, arr[i]);
         }
         System.out.println(list.size());
     }
