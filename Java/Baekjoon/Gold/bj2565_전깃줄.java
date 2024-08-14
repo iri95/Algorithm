2e0f86f2e463777339470e0ba38ab1141e9338b1
LeeSanghun <hoonideal@naver.com>
2024-08-14 09:45:55 +0900

    Gold Binary Search bj2565_전깃줄

--- Java/Baekjoon/Gold/bj2565_전깃줄.java
+++ Java/Baekjoon/Gold/bj2565_전깃줄.java
@@ -13,22 +13,17 @@ public class bj2565_전깃줄 {
             StringTokenizer st = new StringTokenizer(br.readLine());
             list.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
         }
-        Collections.sort(list, (o1, o2) -> {
-            if (o1[0] > o2[0]) return 1;
-            else if (o1[0] < o2[0]) return -1;
-            else return 0;
-        });
+        list.sort(Comparator.comparingInt(o -> o[0]));
         List<Integer> dpList = new ArrayList<>();
         dpList.add(list.get(0)[1]);
         for (int i = 1; i < N; i++) {
-            if (list.get(i)[1] > dpList.get(dpList.size() - 1)) {
-                dpList.add(list.get(i)[1]);
-            }else{
-                int k = Math.abs(Collections.binarySearch(dpList, list.get(i)[1]) + 1);
-                dpList.set(k, list.get(i)[1]);
+            int value = list.get(i)[1];
+            if (value > dpList.get(dpList.size() - 1)) dpList.add(value);
+            else {
+                int k = Math.abs(Collections.binarySearch(dpList, value) + 1);
+                dpList.set(k, value);
             }
         }
         System.out.println(N - dpList.size());
-
     }
 }
