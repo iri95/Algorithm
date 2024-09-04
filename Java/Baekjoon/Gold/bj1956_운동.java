194873b3e849bf349afa3ccd11aead0f0d8f30c4
LeeSanghun <hoonideal@naver.com>
2024-09-04 09:23:10 +0900

    Gold Floyd Warshall bj1956_운동

--- Java/Baekjoon/Gold/bj1956_운동.java
+++ Java/Baekjoon/Gold/bj1956_운동.java
@@ -13,25 +13,27 @@ public class bj1956_운동 {
         int E = Integer.parseInt(st.nextToken());
         int[][] map = new int[V + 1][V + 1];
         int INF = Integer.MAX_VALUE;
-        for (int i = 0; i <= V; i++)
+        for (int i = 0; i <= V; i++) {
             Arrays.fill(map[i], INF);
+            map[i][i] = 0;
+        }
 
-        int ans = Integer.MAX_VALUE;
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            map[a][b] = c;
-            if (a == b) ans = Math.min(ans, c);
+            map[a][b] = Integer.parseInt(st.nextToken());
         }
 
+        int ans = INF;
         for (int m = 1; m <= V; m++)
-            for (int s = 1; s <= V; s++)
-                for (int e = 1; e <= V; e++)
-                    if (map[s][m] != INF && map[m][e] != INF && map[s][e] > map[s][m] + map[m][e])
-                        map[s][e] = map[s][m] + map[m][e];
-
+            for (int s = 1; s <= V; s++) {
+                if (map[s][m] == INF || s == m) continue;
+                for (int e = 1; e <= V; e++) {
+                    if (map[m][e] == INF || m == e) continue;
+                    map[s][e] = Math.min(map[s][e], map[s][m] + map[m][e]);
+                }
+            }
 
         for (int i = 1; i <= V; i++)
             for (int j = i + 1; j <= V; j++)
