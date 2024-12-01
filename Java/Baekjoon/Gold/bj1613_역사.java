9a916b1185ecdaef0fcb73aa9fda048efa5d633f
LeeSanghun <hoonideal@naver.com>
2024-12-01 16:56:45 +0900

    Gold Floyd-Warshall bj1613_역사

---
+++ Java/Baekjoon/Gold/bj1613_역사.java
@@ -0,0 +1,39 @@
+package Gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj1613_역사 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n + 1][n + 1];
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            arr[s][e] = -1;
+            arr[e][s] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) continue;
+                for (int l = 1; l <= n; l++) {
+                    if (arr[j][l] == 0) {
+                        if (arr[j][i] == arr[i][l]) arr[j][l] = arr[j][i];
+                    }
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        int s = Integer.parseInt(br.readLine());
+        for (int i = 0; i < s; i++) {
+            st = new StringTokenizer(br.readLine());
+            sb.append(arr[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
