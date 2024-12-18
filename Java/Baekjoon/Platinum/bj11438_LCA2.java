465e598fb4e5fcf784931b19049ec0381f56b565
LeeSanghun <hoonideal@naver.com>
2024-12-18 13:37:43 +0900

    Platinum LCA + Segment Tree bj11438_LCA2

--- Java/Baekjoon/Platinum/bj11438_LCA2.java
+++ Java/Baekjoon/Platinum/bj11438_LCA2.java
@@ -80,3 +80,93 @@ public class bj11438_LCA2 {
         return parents[a][0];
     }
 }
+
+/*
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class LCA2 {
+
+    static int[] depth, tree;
+    static List<Integer> visit = new ArrayList<>();
+    static List<Integer>[] lists;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        depth = new int[N + 1];
+        depth[0] = Integer.MAX_VALUE;
+        lists = new ArrayList[N + 1];
+        for (int i = 0; i <= N; i++) lists[i] = new ArrayList<>();
+
+        for (int i = 0; i < N - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            lists[a].add(b);
+            lists[b].add(a);
+        }
+        preorder(1, 1, 0);
+
+        int[] order = new int[N + 1];
+        Arrays.fill(order, -1);
+        for (int i = 0; i < visit.size(); i++) {
+            int a = visit.get(i);
+            if(order[a] == -1) order[a] = i;
+        }
+
+        int treeH = (int) Math.ceil(Math.log(visit.size()) / Math.log(2)) + 1;
+        tree = new int[1 << treeH];
+
+        init(1, 0, visit.size() - 1);
+
+        StringBuilder sb = new StringBuilder();
+        int M = Integer.parseInt(br.readLine());
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int s = order[a];
+            int e = order[b];
+            if(s > e){
+                int temp = s;
+                s = e;
+                e = temp;
+            }
+            sb.append(query(1, 0, visit.size() - 1, s, e)).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+
+    private static void preorder(int node, int d, int p) {
+        depth[node] = d;
+        visit.add(node);
+        for (int next : lists[node]) {
+            if (next == p) continue;
+            preorder(next, d + 1, node);
+            visit.add(node);
+        }
+    }
+
+    private static int init(int node, int start, int end) {
+        if (start == end) return tree[node] = visit.get(start);
+        int a = init(node * 2, start, (start + end) / 2);
+        int b = init(node * 2 + 1, (start + end) / 2 + 1, end);
+        return tree[node] = depth[a] > depth[b] ? b : a;
+    }
+
+    private static int query(int node, int start, int end, int findS, int findE){
+        if(findE < start || end < findS) return 0;
+        if(findS <= start && end <= findE) return tree[node];
+        int a = query(node * 2, start, (start + end) / 2, findS, findE);
+        int b = query(node * 2 + 1, (start + end) / 2 + 1, end,  findS, findE);
+        return depth[a] > depth[b] ? b : a;
+    }
+}
+
+ */
