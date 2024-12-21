b9b9509cbf6d4aa3e37086f21d3045efa0123b7d
LeeSanghun <hoonideal@naver.com>
2024-12-21 14:08:24 +0900

    Gold Segment Tree bj1615_교차개수세기

--- Java/Baekjoon/Gold/bj1615_교차개수세기.java
+++ Java/Baekjoon/Gold/bj1615_교차개수세기.java
@@ -3,10 +3,10 @@ package Gold;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
+// java는 메모리 초과가 난다...
 public class bj1615_교차개수세기 {
     private static class SegmentTree {
         int[] tree;
