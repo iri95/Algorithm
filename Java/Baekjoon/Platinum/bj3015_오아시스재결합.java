0881aca711c4b165a03ddfbd649fe62df4592aa8
LeeSanghun <hoonideal@naver.com>
2024-12-26 01:03:39 +0900

    Platinum Stack bj3015_오아시스재결합

--- Java/Baekjoon/Platinum/bj3015_오아시스재결합.java
+++ Java/Baekjoon/Platinum/bj3015_오아시스재결합.java
@@ -14,23 +14,15 @@ public class bj3015_오아시스재결합 {
         for (int i = 0; i < N; i++) {
             int n = Integer.parseInt(br.readLine());
             long cnt = 0;
-            long same = 0;
-            while (!stack.isEmpty()) {
-                long[] peek = stack.peekFirst();
-                cnt++;
-                if (peek[0] > n) break;
-                else if (peek[0] < n) {
-                    long[] pop = stack.pop();
-                    cnt += pop[1];
-                }
-                else {
-                    long[] pop = stack.pop();
-                    same = pop[1] + 1;
-                    cnt += pop[1];
-                }
+            long same = 1;
+            while (!stack.isEmpty() && stack.peekFirst()[0] <= n) {
+                long[] pop = stack.pop();
+                cnt += pop[1];
+                if (pop[0] == n) same = pop[1] + 1;
             }
+            cnt += !stack.isEmpty() ? 1 : 0;
             answer += cnt;
-            stack.push(new long[] {n, same});
+            stack.push(new long[]{n, same});
         }
         System.out.println(answer);
     }
