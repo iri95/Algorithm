bdacb02cea0b16b95a904b01baae5464fc029693
LeeSanghun <hoonideal@naver.com>
2025-02-12 01:29:41 +0900

    Platinum Fenwick Tree bj1517_버블소트

--- Java/Baekjoon/Platinum/bj1517_버블소트.java
+++ Java/Baekjoon/Platinum/bj1517_버블소트.java
@@ -5,10 +5,58 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class bj1517_버블소트 {
-    private static class SegmentTree {
+    public static class FenwickTree {
+        static int[] tree;
+
+        public FenwickTree(int size) {
+            tree = new int[size + 1];
+        }
+
+        long sum(int pos) {
+            long result = 0;
+            while (pos > 0) {
+                result += tree[pos];
+                pos &= (pos - 1);
+            }
+            return result;
+        }
+
+        void add(int pos, int val) {
+            while (pos < tree.length) {
+                tree[pos] += val;
+                pos += (pos & -pos);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Map<Integer, Deque<Integer>> map = new HashMap<>();
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            map.computeIfAbsent(arr[i], k -> new ArrayDeque<>()).push(i);
+        }
+
+        Arrays.sort(arr);
+        FenwickTree fenwick = new FenwickTree(N);
+        long answer = 0;
+        for (int i = N; i > 0; i--) {
+            int index = map.get(arr[i]).pop();
+            answer += fenwick.sum(index - 1);
+            fenwick.add(index, 1);
+        }
+        System.out.println(answer);
+    }
+}
+/* Segment Tree
+private static class SegmentTree {
         int[] tree;
 
-        void init(int n) {
+        SegmentTree(int n) {
             int high = (int) Math.ceil(Math.log(n) / Math.log(2)) + 1;
             tree = new int[1 << high];
         }
@@ -41,9 +89,9 @@ public class bj1517_버블소트 {
         }
 
         Arrays.sort(arr);
-        SegmentTree seg = new SegmentTree();
-        seg.init(N);
+        SegmentTree seg = new SegmentTree(N);
         long answer = 0;
+
         for (int i = 1; i <= N; i++) {
             int index = map.get(arr[i]).poll();
             answer += seg.sum(1, 1, N, index + 1, N);
@@ -51,4 +99,4 @@ public class bj1517_버블소트 {
         }
         System.out.println(answer);
     }
-}
+ */
