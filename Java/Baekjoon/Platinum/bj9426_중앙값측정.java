e613c13e818ac842f714c2153f621a1713e7f7c3
LeeSanghun <hoonideal@naver.com>
2025-03-01 00:24:34 +0900

    Platinum Segment Tree bj9426_중앙값측정

--- Java/Baekjoon/Platinum/bj9426_중앙값측정.java
+++ Java/Baekjoon/Platinum/bj9426_중앙값측정.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
 public class bj9426_중앙값측정 {
     static int N, K, size = 65535;
     static int[] tree;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,7 +22,7 @@ public class bj9426_중앙값측정 {
         int prev = 0;
         int mid = (K + 1) / 2;
         long answer = 0;
-        for(int i = K - 1; i < N; i++){
+        for (int i = K - 1; i < N; i++) {
             update(1, 0, size, arr[i], 1);
             answer += query(1, 0, size, mid);
             update(1, 0, size, arr[prev++], -1);
@@ -29,14 +30,15 @@ public class bj9426_중앙값측정 {
         System.out.println(answer);
 
     }
-    private static int update(int node, int start, int end, int index, int diff){
+
+    private static int update(int node, int start, int end, int index, int diff) {
         if (index < start || end < index) return tree[node];
         if (start == end) return tree[node] += diff;
         return tree[node] = update(node * 2, start, (start + end) / 2, index, diff)
                 + update(node * 2 + 1, (start + end) / 2 + 1, end, index, diff);
     }
 
-    private static int query(int node, int start, int end, int k){
+    private static int query(int node, int start, int end, int k) {
         if (start == end) return start;
 
         if (tree[node * 2] >= k) return query(node * 2, start, (start + end) / 2, k);
