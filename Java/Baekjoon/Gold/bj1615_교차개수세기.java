2fa14b569b933cbbbdd8e76f5d99f715317597d4
LeeSanghun <hoonideal@naver.com>
2025-03-14 00:39:19 +0900

    Gold Segment Tree, Sort, read() bj1615_교차개수세기

--- Java/Baekjoon/Gold/bj1615_교차개수세기.java
+++ Java/Baekjoon/Gold/bj1615_교차개수세기.java
@@ -6,34 +6,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class bj1615_교차개수세기 {
-    private static class SegmentTree {
-        int[] tree;
-
-        private SegmentTree(int N) {
-            int high = (int) Math.ceil(Math.log(N) / Math.log(2)) + 1;
-            tree = new int[1 << high];
-        }
-
-        private int update(int node, int start, int end, int index) {
-            if (index < start || end < index) return tree[node];
-            if (start == end) return tree[node]++;
-            return tree[node] = update(node * 2, start, (start + end) / 2, index)
-                    + update(node * 2 + 1, (start + end) / 2 + 1, end, index);
-        }
-
-        private int sum(int node, int start, int end, int left, int right) {
-            if (right < start || end < left) return 0;
-            if (left <= start && end <= right) return tree[node];
-            return sum(node * 2, start, (start + end) / 2, left, right)
-                    + sum(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
-        }
-    }
+    static int[] tree;
 
     public static void main(String[] args) throws Exception {
         int N = read();
         int M = read();
 
-        SegmentTree seg = new SegmentTree(N);
+        int high = (int) Math.ceil(Math.log(N) / Math.log(2)) + 1;
+        tree = new int[1 << high];
         List<int[]> list = new ArrayList<>();
 
         for (int i = 0; i < M; i++) {
@@ -50,8 +30,8 @@ public class bj1615_교차개수세기 {
         long ans = 0;
 
         for (int[] ij : list) {
-            ans += seg.sum(1, 1, N, ij[1] + 1, N);
-            seg.update(1, 1, N, ij[1]);
+            ans += sum(1, 1, N, ij[1] + 1, N);
+            update(1, 1, N, ij[1]);
         }
 
         System.out.println(ans);
@@ -64,5 +44,25 @@ public class bj1615_교차개수세기 {
             n = (n << 3) + (n << 1) + (i & 15);
 
         return n;
+    }
+
+    private static void update(int node, int start, int end, int index) {
+        if (index < start || end < index) return;
+
+        if (start == end) {
+            tree[node]++;
+            return;
+        }
+        update(node * 2, start, (start + end) / 2, index);
+        update(node * 2 + 1, (start + end) / 2 + 1, end, index);
+
+        tree[node] = tree[node << 1] + tree[node << 1 | 1];
+    }
+
+    private static int sum(int node, int start, int end, int left, int right) {
+        if (right < start || end < left) return 0;
+        if (left <= start && end <= right) return tree[node];
+        return sum(node * 2, start, (start + end) / 2, left, right)
+                + sum(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
     }
 }
