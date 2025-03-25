869aab94c4209598c89d3ab024de6098050175db
LeeSanghun <hoonideal@naver.com>
2025-03-26 00:35:24 +0900

    Platinum Fenwick Tree bj1280_나무심기

--- Java/Baekjoon/Platinum/bj1280_나무심기.java
+++ Java/Baekjoon/Platinum/bj1280_나무심기.java
@@ -6,14 +6,14 @@ import java.io.InputStreamReader;
 public class bj1280_나무심기 {
     static int INF = 1_000_000_007, last = 200_001;
     static long[] fenwickSum = new long[200_002], fenwickCnt = new long[200_002];
-    ;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         long result = 1;
-        int index = Integer.parseInt(br.readLine()) + 1;
 
+        // 첫 나무는 그냥 심는다.
+        int index = Integer.parseInt(br.readLine()) + 1;
         update(fenwickSum, index, index);
         update(fenwickCnt, index, 1);
 
@@ -31,6 +31,7 @@ public class bj1280_나무심기 {
             if (rightCnt != 0) sum += (rightSum - index * rightCnt) % INF;
             result *= sum;
             result %= INF;
+
             update(fenwickSum, index, index);
             update(fenwickCnt, index, 1);
         }
@@ -39,16 +40,15 @@ public class bj1280_나무심기 {
     }
 
     private static void update(long[] fenwickTree, int index, int value) {
-        for (int i = index; i <= last; i += (i & -i)) {
+        for (int i = index; i <= last; i += (i & -i))
             fenwickTree[i] += value;
-        }
     }
 
     private static long sum(long[] fenwickTree, int index) {
         long sum = 0;
-        for (int i = index; i > 0; i -= (i & -i)) {
+        for (int i = index; i > 0; i -= (i & -i))
             sum += fenwickTree[i];
-        }
+
         return sum;
     }
 }
