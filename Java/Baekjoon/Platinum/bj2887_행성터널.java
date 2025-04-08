70ab0b90172658ac6765834fffdb60217d29efa6
LeeSanghun <hoonideal@naver.com>
2025-04-09 01:19:05 +0900

    Platinum Sort, MST bj2887_행성터널

---
+++ Java/Baekjoon/Platinum/bj2887_행성터널.java
@@ -0,0 +1,94 @@
+package Platinum;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class bj2887_행성터널 {
+    private static class Node implements Comparable<Node> {
+        int n, p;
+
+        Node(int n, int p) {
+            this.n = n;
+            this.p = p;
+        }
+
+        public int compareTo(Node n) {
+            return this.p - n.p;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Node[] X = new Node[N];
+        Node[] Y = new Node[N];
+        Node[] Z = new Node[N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            X[i] = new Node(i, Integer.parseInt(st.nextToken()));
+            Y[i] = new Node(i, Integer.parseInt(st.nextToken()));
+            Z[i] = new Node(i, Integer.parseInt(st.nextToken()));
+        }
+
+        Arrays.sort(X);
+        Arrays.sort(Y);
+        Arrays.sort(Z);
+
+        int[] xIndex = new int[N];
+        int[] yIndex = new int[N];
+        int[] zIndex = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            xIndex[X[i].n] = i;
+            yIndex[Y[i].n] = i;
+            zIndex[Z[i].n] = i;
+        }
+
+        boolean[] visited = new boolean[N];
+
+        long sum = 0;
+        int count = 0;
+        Queue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(0, 0));
+        while(count < N){
+            Node cur = pq.poll();
+            if (visited[cur.n]) continue;
+            visited[cur.n] = true;
+            sum += cur.p;
+            count++;
+
+            int x = xIndex[cur.n];
+            int y = yIndex[cur.n];
+            int z = zIndex[cur.n];
+
+            int nx = x;
+            while(nx >= 0 && visited[X[nx].n]) nx--;
+            if (nx >= 0) pq.add(new Node(X[nx].n, Math.abs(X[nx].p - X[x].p)));
+
+            nx = x;
+            while(nx < N && visited[X[nx].n]) nx++;
+            if (nx < N )pq.add(new Node(X[nx].n, Math.abs(X[nx].p - X[x].p)));
+
+            int ny = y;
+            while(ny >= 0 && visited[Y[ny].n]) ny--;
+            if (ny >= 0) pq.add(new Node(Y[ny].n, Math.abs(Y[ny].p - Y[y].p)));
+
+            ny = y;
+            while(ny < N && visited[Y[ny].n]) ny++;
+            if(ny < N) pq.add(new Node(Y[ny].n, Math.abs(Y[ny].p - Y[y].p)));
+
+            int nz = z;
+            while(nz >= 0 && visited[Z[nz].n]) nz--;
+            if (nz >= 0) pq.add(new Node(Z[nz].n, Math.abs(Z[nz].p - Z[z].p)));
+
+            nz = z;
+            while(nz < N && visited[Z[nz].n]) nz++;
+            if (nz < N) pq.add(new Node(Z[nz].n, Math.abs(Z[nz].p - Z[z].p)));
+        }
+        System.out.println(sum);
+    }
+}
