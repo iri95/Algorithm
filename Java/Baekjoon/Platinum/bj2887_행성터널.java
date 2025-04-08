c0f7c3d157cfc558a216dd5891d7e0ba462dedd5
LeeSanghun <hoonideal@naver.com>
2025-04-09 02:09:05 +0900

    Platinum Sort, MST bj2887_행성터널

--- Java/Baekjoon/Platinum/bj2887_행성터널.java
+++ Java/Baekjoon/Platinum/bj2887_행성터널.java
@@ -2,10 +2,7 @@ package Platinum;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class bj2887_행성터널 {
     private static class Node implements Comparable<Node> {
@@ -21,68 +18,78 @@ public class bj2887_행성터널 {
         }
     }
 
+    private static class Edge implements Comparable<Edge> {
+        int start, end, cost;
+
+        Edge(int s, int e, int c) {
+            this.start = s;
+            this.end = e;
+            this.cost = c;
+        }
+
+        public int compareTo(Edge e) {
+            return this.cost - e.cost;
+        }
+    }
+
+    static int[] parent;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Node[] X = new Node[N];
-        Node[] Y = new Node[N];
-        Node[] Z = new Node[N];
+        parent = new int[N];
+        List<Node> X = new ArrayList<>();
+        List<Node> Y = new ArrayList<>();
+        List<Node> Z = new ArrayList<>();
+
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            X[i] = new Node(i, Integer.parseInt(st.nextToken()));
-            Y[i] = new Node(i, Integer.parseInt(st.nextToken()));
-            Z[i] = new Node(i, Integer.parseInt(st.nextToken()));
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+            X.add(new Node(i, x));
+            Y.add(new Node(i, y));
+            Z.add(new Node(i, z));
+            parent[i] = i;
         }
 
-        Arrays.sort(X);
-        Arrays.sort(Y);
-        Arrays.sort(Z);
+        Collections.sort(X);
+        Collections.sort(Y);
+        Collections.sort(Z);
 
-        int[] xIndex = new int[N];
-        int[] yIndex = new int[N];
-        int[] zIndex = new int[N];
+        Queue<Edge> pq = new PriorityQueue<>();
 
-        for (int i = 0; i < N; i++) {
-            xIndex[X[i].n] = i;
-            yIndex[Y[i].n] = i;
-            zIndex[Z[i].n] = i;
+        for (int i = 1; i < N; i++) {
+            pq.add(new Edge(X.get(i - 1).n, X.get(i).n, Math.abs(X.get(i - 1).p - X.get(i).p)));
+            pq.add(new Edge(Y.get(i - 1).n, Y.get(i).n, Math.abs(Y.get(i - 1).p - Y.get(i).p)));
+            pq.add(new Edge(Z.get(i - 1).n, Z.get(i).n, Math.abs(Z.get(i - 1).p - Z.get(i).p)));
         }
 
-        boolean[] visited = new boolean[N];
-
-        long sum = 0;
         int count = 0;
-        Queue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(0, 0));
-        while(count < N){
-            Node cur = pq.poll();
-            if (visited[cur.n]) continue;
-            visited[cur.n] = true;
-            sum += cur.p;
-            count++;
-
-            int x = xIndex[cur.n];
-            int y = yIndex[cur.n];
-            int z = zIndex[cur.n];
-
-            int nx = x - 1;
-            if (nx >= 0 && !visited[X[nx].n]) pq.add(new Node(X[nx].n, Math.abs(X[nx].p - X[x].p)));
-
-            nx = x + 1;
-            if (nx < N && !visited[X[nx].n])pq.add(new Node(X[nx].n, Math.abs(X[nx].p - X[x].p)));
-
-            int ny = y - 1;
-            if (ny >= 0 && !visited[Y[ny].n]) pq.add(new Node(Y[ny].n, Math.abs(Y[ny].p - Y[y].p)));
-
-            ny = y + 1;
-            if(ny < N && !visited[Y[ny].n]) pq.add(new Node(Y[ny].n, Math.abs(Y[ny].p - Y[y].p)));
-
-            int nz = z - 1;
-            if (nz >= 0 && !visited[Z[nz].n]) pq.add(new Node(Z[nz].n, Math.abs(Z[nz].p - Z[z].p)));
-
-            nz = z + 1;
-            if (nz < N && !visited[Z[nz].n]) pq.add(new Node(Z[nz].n, Math.abs(Z[nz].p - Z[z].p)));
+        long sum = 0;
+        while(count < N - 1){
+            Edge cur = pq.poll();
+            if (union(cur.start, cur.end)) {
+                sum += cur.cost;
+                count++;
+            }
         }
         System.out.println(sum);
+    }
+
+    private static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    private static boolean union(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+
+        if (px == py) return false;
+
+        if (px < py)parent[py] = px;
+        else parent[px] = py;
+        return true;
     }
 }
