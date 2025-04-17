0c793a13420d7ee561ebcb6eab193295aa6bd3a1
LeeSanghun <hoonideal@naver.com>
2025-04-18 01:35:25 +0900

    LV4 DP 올바른괄호의개수

---
+++ Java/Programmers/lv4/올바른괄호의개수.java
@@ -0,0 +1,17 @@
+package lv4;
+
+public class 올바른괄호의개수 {
+    public int solution(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+
+        return dp[n];
+    }
+}
