63a07d8976bd920596578c1d57d415bc9df1698b
LeeSanghun <hoonideal@naver.com>
2025-04-21 01:34:44 +0900

    LV3 Binary Search 선입선출스케줄링

---
+++ Java/Programmers/lv3/선입선출스케줄링.java
@@ -0,0 +1,37 @@
+package lv3;
+
+import java.util.*;
+
+public class 선입선출스케줄링 {
+    public int solution(int n, int[] cores) {
+        if (n <= cores.length)
+            return n;
+
+        int answer = 0;
+        int s = 0;
+        int e = 500_000_001;
+        while (s < e) {
+            int mid = (s + e) / 2;
+            long sum = cores.length;
+
+            for (int core : cores)
+                sum += mid / core;
+
+            if (sum >= n)
+                e = mid;
+            else
+                s = mid + 1;
+        }
+
+        for (int core : cores)
+            n -= (e - 1) / core + 1;
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < cores.length; i++)
+            if (e % cores[i] == 0)
+                list.add(i + 1);
+
+        return list.get(n - 1);
+    }
+}
