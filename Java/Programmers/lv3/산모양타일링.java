a6439f8ccd6f1545d44efe349f1770a805b7d1d7
LeeSanghun <hoonideal@naver.com>
2025-05-02 01:23:23 +0900

    LV3 DP 산모양타일링

---
+++ Java/Programmers/lv3/산모양타일링.java
@@ -0,0 +1,26 @@
+package lv3;
+
+public class 산모양타일링 {
+    public int solution(int n, int[] tops) {
+        int div = 10_007;
+        int[] dp = new int[4];
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            int all = dp[0] + dp[1] + dp[2] + dp[3];
+
+            dp[1] = (all - dp[2]) % div;
+            dp[0] = all % div;
+            dp[2] = all % div;
+            if (tops[i - 1] == 1) {
+                dp[3] = all % div;
+            } else dp[3] = 0;
+        }
+
+        int answer = 0;
+        for (int x : dp)
+            answer = (answer + x) % div;
+
+        return answer;
+    }
+}
