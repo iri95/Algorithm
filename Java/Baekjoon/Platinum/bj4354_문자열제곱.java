ad8af30c6f98a36b4e243a506cc483fa36d609d4
LeeSanghun <hoonideal@naver.com>
2025-05-14 01:05:12 +0900

    Platinum KMP bj4354_문자열제곱

--- Java/Baekjoon/Platinum/bj4354_문자열제곱.java
+++ Java/Baekjoon/Platinum/bj4354_문자열제곱.java
@@ -7,26 +7,26 @@ public class bj4354_문자열제곱 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        while(true){
+        int[] table;
+        int idx, aLen, n;
+        while (true) {
             String str = br.readLine();
             if (str.equals(".")) break;
-            int[] table = new int[str.length()];
-            int idx = 0;
-            int aLen = 1;
-            for (int i = 1; i < str.length(); i++) {
-                if (str.charAt(i) != str.charAt(idx)) {
-                    if (table[i - 1] == 1)  aLen = i;
-                    else aLen = i + 1;
-                }
+            n = str.length();
+            table = new int[n];
+            idx = 0;
 
-                while(idx > 0 && str.charAt(i) != str.charAt(idx))
-                    idx = table[idx - 1];
+            for (int i = 1; i < n; i++) {
+                if (idx > 0 && str.charAt(i) != str.charAt(idx))
+                    idx = 0;
 
                 if (str.charAt(i) == str.charAt(idx))
                     table[i] = ++idx;
             }
-            if (str.length() % aLen != 0) sb.append(1).append("\n");
-            else sb.append(str.length() / aLen).append("\n");
+
+            aLen = n - table[n - 1];
+            if (n % aLen != 0) sb.append(1).append("\n");
+            else sb.append(n / aLen).append("\n");
         }
 
         System.out.println(sb);
